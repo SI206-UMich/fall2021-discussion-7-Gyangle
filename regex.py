b05783fce673cc938d39b87896194bc916eb7e68
grant <ley5@uw.edu>
2021-10-13 14:23:59 -0400

    finish third

--- regex.py
+++ regex.py
@@ -4,64 +4,77 @@ import unittest
 
 def read_file(filename):
     """ Return a list of the lines in the file with the passed filename """
-    
+
     # Open the file and get the file object
     source_dir = os.path.dirname(__file__) #<-- directory name
     full_path = os.path.join(source_dir, filename)
     infile = open(full_path,'r', encoding='utf-8')
-    
+
     # Read the lines from the file object into a list
     lines = infile.readlines()
-    
+
     # Close the file object
     infile.close()
-    
+
     # return the list of lines
     return lines
 
 def find_word(string_list):
     """ Return a list of words that contain three digit numbers in the middle. """
-
     # initialize an empty list
-
+    list = []
     # define the regular expression
-
-    # loop through each line of the string list 
-
-    # find all the words that match the regular expression in each line
-    
-    # loop through the found words and add the words to your empty list 
-
-    #return the list of all words that start with the letter B, E, or T
-    pass
+    reg = "\\b[A-Qa-z]+\d{3}[A-Qa-z]+"
+    # loop through each line of the string list
+    for line in string_list:
+        # find all the words that match the regular expression in each line
+        if (len(re.findall(reg, line))):
+        # loop through the found words and add the words to your empty list
+            list.append(re.findall(reg, line)[0])
+    # print(re.findall(reg, string_list))
+    return list
 
 
 def find_days(string_list):
-    """ Return a list of days from the list of strings the dates format in the text are MM/DD/YYYY. """  
+    """ Return a list of days from the list of strings the dates format in the text are MM/DD/YYYY. """
 
     # initialize an empty list
-
+    list = []
     # define the regular expression
-
+    reg = "\\b\d+\/\d+\/\d{4}"
     # loop through each line of the string list
-    
-    # find all the dates that match the regular expression in each line
-    
-    # loop through the found dates and only add the days to your empty list 
-    
+    for line in string_list:
+        # find all the dates that match the regular expression in each line
+        if (len(re.findall(reg, line))):
+          # loop through the found words and add the words to your empty list
+            str = re.findall(reg, line)[0]
+            str = str[str.find("/") + 1 :]
+            str = str[:str.find("/")]
+            list.append(str)
+    # loop through the found dates and only add the days to your empty list
+    return list
     #return the list of days
-    pass
 
 def find_domains(string_list):
     """ Return a list of web address domains from the list of strings the domains of a wbsite are after www. """
 
     # initialize an empty list
-
+    list = []
     # define the regular expression
-
+    # reg = "\\b[w]{3}[.]{1}.*"
+    reg= r"\bhttps?://.*"
     # loop through each line of the string list
+    for line in string_list:
+        # find all the domains that match the regular expression in each line
+        if (len(re.findall(reg, line))):
+          # loop through the found words and add the words to your empty list
+            str = re.findall(reg, line)[0]
+            str = str.replace("https://", "")
+            str = str.replace("http://", "")
+            str = str.replace("www.", "")
+            str = str.replace("/", "")
+            list.append(str)
 
-    # find all the domains that match the regular expression in each line
 
     # loop through the found domains
 
@@ -69,9 +82,9 @@ def find_domains(string_list):
     # then strip the www. to get only the domain name
 
     # add the domains to your empty list
-    
+
     #return the list of domains
-    pass
+    return list
 
 class TestAllMethods(unittest.TestCase):
 
@@ -81,13 +94,13 @@ class TestAllMethods(unittest.TestCase):
         string_list = read_file('alice_ch_1.txt')
         word_list = find_word(string_list)
         self.assertEqual(len(word_list),4)
-    
+
     def test_find_days(self):
         # read the lines from the file into a list of strings
         string_list = read_file('alice_ch_1.txt')
         days_list = find_days(string_list)
         self.assertEqual(days_list,['23', '12', '31', '4', '1', '4'])
-    
+
     def test_domains(self):
         # read the lines from the file into a list of strings
         string_list = read_file('alice_ch_1.txt')
@@ -96,7 +109,7 @@ class TestAllMethods(unittest.TestCase):
 
 
 def main():
-	# Use main to test your function. 
+	# Use main to test your function.
     # Run unit tests, but feel free to run any additional functions you need
 	unittest.main(verbosity = 2)
 
